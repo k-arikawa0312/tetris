ac3a6254ba78f6da915fa9671dcd5c7401d6727b
k-arikawa0312 <s1F102402710@iniad.org>
2024-05-22 22:36:56 +0900

    固定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,66 @@
 import styles from './index.module.css';
 import React, { useState } from 'react';
 
+const changeBlock = (board: number[][], position: number[][], toChange: number) => {
+  const newBoard = structuredClone(board);
+  for (const [tx, ty] of position) {
+    if (newBoard[ty] !== undefined) newBoard[ty][tx] = toChange;
+  }
+  return newBoard;
+};
+
+const fallBlock = (board: number[][]) => {
+  const position = [];
+  const block = [];
+  for (let y = 0; y < 20; y++) {
+    for (let x = 0; x < 10; x++) {
+      for (let nx = 0; nx < 10; nx++) {
+        for (let ny = 0; ny < 20; ny++) {
+          if (board[ny][nx] === 1) {
+            block.push([nx, ny]);
+            if (board[ny + 1][nx] === 2) {
+              for (let ty = 0; ty < 20; ty++) {
+                for (let tx = 0; tx < 10; tx++) {
+                  if (board[ty] !== undefined && board[ty][tx] === 1) {
+                    board[ty][tx] = 2;
+                  }
+                }
+              }
+
+              return board;
+            }
+          }
+        }
+      }
+      if (board[y][x] === 1 && board[y + 1]?.[x] !== 2) {
+        position.push([x, y + 1]);
+        if (y === 19) {
+          console.table(board);
+          for (const [tx, ty] of block) {
+            if (board[ty] !== undefined) {
+              board[ty][tx] = 2;
+            }
+          }
+          return board;
+        }
+        if (board[y + 1]?.[x] === 2) {
+          for (const [tx, ty] of block) {
+            if (board[ty] !== undefined) {
+              board[ty][tx] = 2;
+            }
+          }
+        }
+        board[y][x] = 0;
+      }
+    }
+  }
+  return changeBlock(board, position, 1);
+};
+
 const Home = () => {
   const [board, setBoard] = useState([
+    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -20,28 +78,30 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 0, 0, 0, 0, 0],
   ]);
-  const clickHandler = (x: number, y: number) => {
-    board[y][x] = 1;
-    setBoard(board);
-    console.log(board);
+
+  const click = () => {
+    const aaa = structuredClone(board);
+    const newBoard = fallBlock(aaa);
+
+    setBoard(newBoard);
   };
+
+  console.log(board);
   return (
     <div className={styles.container}>
+      <button onClick={click}>落ちる</button>
       <div className={styles.backBoard}>
         {board.map((row, y) =>
           row.map((display, x) => (
-            <div className={styles.cell} key={`${x}-${y}`} onClick={() => clickHandler(x, y)}>
-              {display === 3 && <div className={styles.area} />}
-              {display !== 0 && display !== 3 && (
-                <div
-                  className={styles.stone}
-                  style={{ background: display === 1 ? '#0080ff' : '#a31414' }}
-                />
-              )}
+            <div className={styles.cell} key={`${x}-${y}`}>
+              <div
+                className={styles.stone}
+                style={{
+                  background: display === 0 ? '#000000' : display === 1 ? '#0084ff' : '#d9ff00',
+                }}
+              />
             </div>
           )),
         )}
